26e85f9049ae1b89a785f586bf32fde6bd111064
Isaac Joshua <isaacjcarnes@gmail.com>
2021-09-25 13:39:29 -0400

    Added timer function, currently not called by anything

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ const underscore = document.getElementById("underscores");
 const timer = document.getElementById("timer");
 const win = document.getElementById("winStat");
 const loss = document.getElementById("lossStat");
+
+
+function gameTimer() {
+    var timeLimit = 45;
+  
+    var timeInterval = setInterval(function () {
+      timeLimit--;
+      console.log(timeLimit);
+      
+      timer.innerHTML = `${timeLimit} seconds left`;
+      if(timeLimit <= 0){
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+}
